7840c5ab0001bd6402a9837377d1bc8789723912
Vadim Sakhno <vadim@onehopewine.com>
2022-06-13 13:25:37 +0300

    Refactoring

--- application.py
+++ application.py
@@ -5,9 +5,7 @@ print("One more change")
 "New line"
 "New line"
 "Bug Fix"
-"Testing git two people working"
-<<<<<<< HEAD
+"Testing git two people 
 "Second developer made changes'
-=======
-"First developer made changes"
->>>>>>> beb589d1beffc0e03e499952a76fd8c30848e3a8
+
+"Another update"
